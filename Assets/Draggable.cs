9879c76c9650dd709119f37f913d7a7ff4aaf373
Jack Ross, III <sxe4ever@gmail.com>
2017-09-30 10:32:35 -0400

    Now detecting collisions

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -4,19 +4,42 @@ using System.Collections;
 
 public class Draggable : MonoBehaviour {
 
-    private Vector3 offset;
+  private Vector3 offset;
 
-    void OnMouseDown()
-    {
-			print("Mouse down");
-        offset = gameObject.transform.position -
-            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
-    }
+  // Starting X position before moved
+  protected float initialX;
+  // Starting Y position before moved
+  protected float initialY;
 
-    void OnMouseDrag()
-    {
-			print("Mouse drag");
-        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
-        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
-    }
+  protected Vector3 screenPoint;
+
+  void OnMouseDown()
+  {
+      this.initialX = transform.position.x;
+      this.initialY = transform.position.y;
+      offset = gameObject.transform.position -
+          Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+  }
+
+  void OnMouseDrag()
+  {
+      Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+      transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+  }
+
+  void OnMouseUp() {
+    transform.position = new Vector3(initialX, initialY);
+  }
+
+  void OnCollisionEnter2D (Collision2D collision) {
+    print("Collission detected");
+  }
+
+  void OnCollisionStay2D (Collision2D collission) {
+    print("Staying");
+  }
+
+  void OnTriggerEnter (Collider collisionObject) {
+    print("Whoa!");
+  }
 }
