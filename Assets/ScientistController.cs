40db55fc373dea621fee47caf4cac9103b3df893
Jack Ross, III <sxe4ever@gmail.com>
2017-10-01 10:49:34 -0400

    Implemented real background in the actual game

--- Assets/ScientistController.cs
+++ Assets/ScientistController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class ScientistController : MonoBehaviour {
 
 	public GlobalContainer globalContainer;
+	public SpriteRenderer pulsingBackground;
+	public float flashRate = 0f;
+
+	protected float opacity = 0f;
+	protected bool flashAscending = true;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +19,16 @@ public class ScientistController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+		pulsingBackground.color = new Color(1f, 1f, 1f, this.opacity);
+		if(this.opacity >= 0.85) {
+			this.flashAscending = false;
+		} else if (this.opacity < 0.15f) {
+			this.flashAscending = true;
+		}
+		if(this.flashAscending) {
+			this.opacity += this.flashRate;
+		} else {
+			this.opacity -= this.flashRate;
+		}
 	}
 }
