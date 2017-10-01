ed7c9bfe19d0980a41a4eb425b74d7a63715057d
Jack Ross, III <sxe4ever@gmail.com>
2017-09-30 20:27:47 -0400

    Added background for staging area

---
+++ Assets/StartButtonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButtonController : MonoBehaviour {
+
+	public Button yourButton;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void TaskOnClick() {
+		SceneManager.LoadScene(
+			"Main Level",
+			LoadSceneMode.Single
+			);
+	}
+}
