846d346def67831b265e1695f35933ece2f7e45b
Jack Ross, III <mrjackross@gmail.com>
2017-12-17 11:58:34 -0500

    [#19] Fixed issue with not being able to use toggle music button after leaving main menu and then returning

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,15 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour {
 	// Object that contains the music player script
 	public MusicPlayerScript musicPlayer;
-	// Button pressed when they are toggling the music on/off
-	public Text toggleMusicButton;
 
 	protected SettingsController settings = null;
+	protected MuteMusicButton muteMusicButton = null;
 	private static MenuController instance = null;
 
 	public void Update() {
@@ -22,7 +20,6 @@ public class MenuController : MonoBehaviour {
 		if(instance != null) {
 			Destroy(gameObject);
 		} else {
-			print("Continuing");
 			instance = this;
 			DontDestroyOnLoad (gameObject);
 			this.settings = new SettingsController();
@@ -30,19 +27,31 @@ public class MenuController : MonoBehaviour {
 		}
 	}
 
+	public bool isMusicPlaying() {
+		return settings.musicPlaying;
+	}
+
 	public void toggleMusic() {
-		print("Toggle music called");
 		if(settings.musicPlaying) {
 			this.musicPlayer.stopMusic();
-			if(this.toggleMusicButton != null) {
-				this.toggleMusicButton.text = "Play Music";
-			}
 		} else {
 			this.musicPlayer.startMusic();
-			if(this.toggleMusicButton != null) {
-				this.toggleMusicButton.text = "Stop Music";
-			}
 		}
 		settings.musicPlaying = !settings.musicPlaying;
+		this.searchForMuteMusicButton();
+		if(this.muteMusicButton != null) {
+			this.muteMusicButton.setText();
+		}
+	}
+
+	protected void searchForMuteMusicButton() {
+		if(this.muteMusicButton != null) {
+			return;
+		}
+		GameObject button = GameObject.FindGameObjectWithTag("toggleMusicButton");
+		if(button == null) {
+			return;
+		}
+		this.muteMusicButton = button.GetComponent<MuteMusicButton>();
 	}
 }
---
+++ Assets/Scripts/MuteMusicButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteMusicButton : MonoBehaviour {
+
+	// Button pressed when they are toggling the music on/off
+	public Text toggleMusicButton;
+
+	private MenuController menuController = null;
+
+	public void Start() {
+		this.setText();
+	}
+
+	public void toggleMusic() {
+		this.findMenuController();
+		this.menuController.toggleMusic();
+	}
+
+	public void setText() {
+		this.findMenuController();
+		if(this.menuController.isMusicPlaying()) {
+			this.toggleMusicButton.text = "Stop Music";
+		} else {
+			this.toggleMusicButton.text = "Play Music";
+		}
+	}
+
+	protected void findMenuController() {
+		if(this.menuController == null) {
+			this.menuController = GameObject.FindGameObjectWithTag("menuController").GetComponent<MenuController>();
+		}
+	}
+}
