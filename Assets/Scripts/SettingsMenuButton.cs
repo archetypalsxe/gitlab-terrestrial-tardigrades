8051153bd7249bc894bc61110dd29ff148421f02
Jack Ross, III <mrjackross@gmail.com>
2018-01-16 20:37:28 -0500

    [#18] Working on getting settings panel to display/hide. Can hide, but once displayed, not able to get hidden again

---
+++ Assets/Scripts/SettingsMenuButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenuButton : MonoBehaviour {
+
+	public GameObject settingsPanel;
+
+	// Use this for initialization
+	void Start () {
+		this.HidePanel();
+	}
+
+	void DisplayPanel () {
+		this.settingsPanel.SetActive(true);
+		this.settingsPanel.GetComponent<CanvasGroup>().alpha = 1;
+	}
+
+	void HidePanel () {
+		this.settingsPanel.SetActive(false);
+		this.settingsPanel.GetComponent<CanvasGroup>().alpha = 0;
+	}
+
+	public void TogglePanel () {
+		if(this.settingsPanel.activeSelf) {
+			this.HidePanel();
+		} else {
+			this.DisplayPanel();
+		}
+	}
+}
