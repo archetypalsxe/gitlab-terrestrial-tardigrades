8d85a9236cd60fbf376fcc4d7c0d3e29330346b9
Jack Ross, III <mrjackross@gmail.com>
2018-01-23 20:46:27 -0500

    [#18] Hiding/displaying settings panel was magically working on Linux laptop. Resolved issue when game started on tutorial page rather than title screen

--- Assets/Scripts/SettingsMenuButton.cs
+++ Assets/Scripts/SettingsMenuButton.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenuButton : MonoBehaviour {
 
 	public GameObject settingsPanel;
+	private MenuController menuController = null;
+	private Toggle toggleButton = null;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +15,25 @@ public class SettingsMenuButton : MonoBehaviour {
 	}
 
 	void DisplayPanel () {
+		if(this.menuController == null) {
+			GameObject gameObject = GameObject.FindGameObjectWithTag("menuController");
+			if(gameObject != null) {
+				this.menuController = gameObject.GetComponent<MenuController>();
+			} else {
+				this.menuController = new MenuController();
+			}
+		}
+
 		this.settingsPanel.SetActive(true);
+
+		if(this.toggleButton == null) {
+			this.toggleButton = GameObject.Find("MusicToggle").GetComponent<Toggle>();
+		}
+
+		if(this.toggleButton != null && this.menuController != null) {
+			this.toggleButton.isOn = menuController.isMusicPlaying();
+		}
+
 		this.settingsPanel.GetComponent<CanvasGroup>().alpha = 1;
 	}
 
